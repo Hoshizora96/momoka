7c5b101b4802becd231c2b1fe035e41eab189ed8
DrakMastery <553528624@qq.com>
2017-10-27 13:21:36 +0800

    Add double jump

--- momoka/src/fsm/hero/FallingState.cpp
+++ momoka/src/fsm/hero/FallingState.cpp
@@ -2,6 +2,7 @@
 #include "fsm/hero/FallingState.h"
 #include "fsm/hero/StandState.h"
 #include "util/Log.h"
+#include "fsm/hero/JumpState.h"
 
 FallingState::FallingState(Hero& hero)
 	: HeroState(hero) {
@@ -15,9 +16,25 @@ HeroState* FallingState::Onland() {
 	return new StandState(m_hero_);
 }
 
+
+HeroState* FallingState::JumpKeyState(INPUT_KEY_EVENT keyEvent) {
+	if (keyEvent == Key_press) {
+		int jumpnum = m_hero_.GetJumpNum();
+		if (jumpnum > 0) {
+			m_hero_.SetJumpNum(jumpnum - 1);
+			return new JumpState(m_hero_);
+		}
+		return new FallingState(m_hero_);
+	}
+	return HeroState::JumpKeyState(keyEvent);
+}
+
 HeroState* FallingState::Update() {
 	if (m_hero_.GetVelocityY() < 1300.f) {
 		m_hero_.SetVelocityY(m_hero_.GetVelocityY() + 200);
+	}
+	if (m_hero_.GetY() > 800) {
+		return nullptr;
 	}
 	return HeroState::Update();
 }
--- momoka/src/objects/Hero.cpp
+++ momoka/src/objects/Hero.cpp
@@ -5,7 +5,7 @@
 #include "fsm/hero/StandState.h"
 #include "services/InputService.h"
 
-Hero::Hero(): m_defaultHorizontalVelocity_(400.f), m_state_(new StandState(*this)) {
+Hero::Hero() : m_defaultHorizontalVelocity_(400.f), m_state_(new StandState(*this)) {
 	SecureZeroMemory(&m_physicalBody_, sizeof(PhysicalBody));
 	SecureZeroMemory(&m_nextFramePhysicalBody_, sizeof(PhysicalBody));
 
@@ -22,27 +22,27 @@ void Hero::Render(float dt) {
 	float x = m_physicalBody_.posX + m_physicalBody_.velocityX * (dt / 1000);
 	float y = m_physicalBody_.posY + m_physicalBody_.velocityY * (dt / 1000);
 
-		float next_x = m_nextFramePhysicalBody_.posX;
-		float next_y = m_nextFramePhysicalBody_.posY;
+	float next_x = m_nextFramePhysicalBody_.posX;
+	float next_y = m_nextFramePhysicalBody_.posY;
 
-//		if (x > next_x && m_physicalBody_.velocityX > 0 || x < next_x && m_physicalBody_.velocityX < 0) {
-//			x = next_x;
-//		}
-//	
-//		if (y > next_y && m_physicalBody_.velocityY > 0 || y < next_y && m_physicalBody_.velocityY < 0) {
-//			y = next_y;
-//		}
+	//		if (x > next_x && m_physicalBody_.velocityX > 0 || x < next_x && m_physicalBody_.velocityX < 0) {
+	//			x = next_x;
+	//		}
+	//	
+	//		if (y > next_y && m_physicalBody_.velocityY > 0 || y < next_y && m_physicalBody_.velocityY < 0) {
+	//			y = next_y;
+	//		}
 
 	pGraphicService->DrawRect(x, y, momoka_global::TILE_SIZE, momoka_global::TILE_SIZE * 2);
 
 }
 
 void Hero::MoveLeft() {
-	
+
 }
 
 void Hero::MoveRight() {
-	
+
 }
 
 void Hero::MoveUp() {
@@ -54,7 +54,7 @@ void Hero::MoveDown() {
 }
 
 void Hero::Jump() {
-	
+
 }
 
 
@@ -79,14 +79,14 @@ void Hero::HandleInput() {
 	const auto pInputService = Engine::m_serviceLoader.LocateService<InputService>(SERVICE_TYPE::Service_input).
 		lock();
 
-	if(pInputService->IsKeyEventHappened(DIK_A, Key_down)) {
+	if (pInputService->IsKeyEventHappened(DIK_A, Key_down)) {
 		SwitchState(m_state_->LeftKeyState(Key_down));
 	}
-	if(pInputService->IsKeyEventHappened(DIK_A, Key_release)) {
+	if (pInputService->IsKeyEventHappened(DIK_A, Key_release)) {
 		SwitchState(m_state_->LeftKeyState(Key_release));
 	}
 
-	if(pInputService->IsKeyEventHappened(DIK_D, Key_down)) {
+	if (pInputService->IsKeyEventHappened(DIK_D, Key_down)) {
 		SwitchState(m_state_->RightKeyState(Key_down));
 	}
 	if (pInputService->IsKeyEventHappened(DIK_D, Key_release)) {
@@ -101,13 +101,21 @@ void Hero::HandleInput() {
 	}
 }
 
+int Hero::GetJumpNum() const {
+	return this->m_jumpnumber_;
+}
+
+void Hero::SetJumpNum(int num) {
+	m_jumpnumber_ = num;
+}
+
 void Hero::Update() {
 	HandleInput();
 	auto dt = 1 / Engine::m_refreshRate;
 	m_physicalBody_.posX += m_physicalBody_.velocityX * dt;
 	m_physicalBody_.posY += m_physicalBody_.velocityY * dt;
 	//	m_physicalBody_=m_nextFramePhysicalBody_;
-	
+
 
 	SwitchState(m_state_->Update());
 	if (m_pCollisionDetector_ != nullptr) {
@@ -126,13 +134,13 @@ void Hero::Update() {
 		}
 	}
 
-//		if (m_pCollisionDetector_ != nullptr) {
-//			m_nextFramePhysicalBody_ = m_physicalBody_;
-//			m_nextFramePhysicalBody_.posX += m_physicalBody_.velocityX * dt;
-//			m_nextFramePhysicalBody_.posY += m_physicalBody_.velocityY * dt;
-//			auto tileCollisionVector = m_pCollisionDetector_->TileCollisionChecker(m_nextFramePhysicalBody_);
-//			for (auto tileCollision : tileCollisionVector) {
-//				m_nextFramePhysicalBody_ = m_pCollisionDetector_->TileCollisionDefaultSolver(tileCollision, m_nextFramePhysicalBody_);
-//			}
-//		}
+	//		if (m_pCollisionDetector_ != nullptr) {
+	//			m_nextFramePhysicalBody_ = m_physicalBody_;
+	//			m_nextFramePhysicalBody_.posX += m_physicalBody_.velocityX * dt;
+	//			m_nextFramePhysicalBody_.posY += m_physicalBody_.velocityY * dt;
+	//			auto tileCollisionVector = m_pCollisionDetector_->TileCollisionChecker(m_nextFramePhysicalBody_);
+	//			for (auto tileCollision : tileCollisionVector) {
+	//				m_nextFramePhysicalBody_ = m_pCollisionDetector_->TileCollisionDefaultSolver(tileCollision, m_nextFramePhysicalBody_);
+	//			}
+	//		}
 }
