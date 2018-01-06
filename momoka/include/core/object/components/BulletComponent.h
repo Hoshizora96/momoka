5f6c9e0a6d8b2259e62a1dfeadcbe80095a4ed68
DrakMastery <553528624@qq.com>
2018-01-06 18:09:14 +0800

    真正实现了子弹的飞行时间过长会自动消失

--- momoka/include/core/object/components/BulletComponent.h
+++ momoka/include/core/object/components/BulletComponent.h
@@ -3,7 +3,9 @@
 
 class BulletComponent :public Component {
 public:
+	int bulletType = 0;
 	float obstacleHeight = momoka::BULLET_SIZE;
 	float obstacleWidth = momoka::BULLET_SIZE;
 	float damage = 2 * momoka::DAMAGE_UNIT;
+	float MaxFlyTime = 3;
 };
