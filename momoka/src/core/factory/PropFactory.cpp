c8f18e7751dfb8b1608774987ae892f3b051a75d
DrakMastery <553528624@qq.com>
2018-01-06 18:01:50 +0800

    Revert "Revert "添加了击退弹，但未实现击退弹的功能""
    This reverts commit 8621e457ce76d3b47db7e65c53ee96e5156393b3.

--- momoka/src/core/factory/PropFactory.cpp
+++ momoka/src/core/factory/PropFactory.cpp
@@ -26,9 +26,11 @@ GameEntityPool::Entity PropFactory::Create(GameEntityPool& pool, int propIndex) 
 		propCom.PropIndex = 0;
 		break;
 	case 1:
+		break;
+	case 2:
 		positionCom.x = 6 * momoka::TILE_SIZE;
 		positionCom.y = momoka::TILE_SIZE;
-		propCom.PropIndex = 1;
+		propCom.PropIndex = 2;
 		break;
 	default:
 		break;
